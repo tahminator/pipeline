ec81a7de9e143fe93455eb2ee6ee4d2c1541b26f
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-29 02:06:06 -0400

    Remove @beta off SonarScannerClient & add proper comments to types & methods to explain usages and provide examples

--- src/sonar/client.ts
+++ src/sonar/client.ts
@@ -7,28 +7,58 @@ type SonarScannerOpts = {
     token: string;
   };
   run: {
+    /**
+     * Pass in a Bun command
+     *
+     * @example
+     * ```
+     * new SonarScannerClient({
+     *   run: {
+     *     runTestsCmd: $`./mvnw clean verify -Dspring.profiles.active=ci`
+     *   }
+     * })
+     * ```
+     */
     runTestsCmd: $.ShellPromise;
   };
   scan: {
     hostUrl?: string;
+    /**
+     * All args will be formated into `-Dsonar.${key}=${value}`
+     */
     additionalArgs?: Record<string, string | number | boolean>;
     projectKey: string;
     organization: string;
+    /**
+     * Point this at where your source code files reside. e.g. `src/` or `src/java`
+     */
     sourceCodeDir: string;
   };
 };
 
 /**
- * @beta WIP
+ * Provides a unified way to run tests & upload test coverage to a SonarQube instance.
+ *
+ * This client can be used to wire up test coverage for any language & environment; essentially, if SonarQube
+ * supports it, this client can do it.
  */
 export class SonarScannerClient {
+  /**
+   * Get `token`, `projectKey` & `organization` from https://sonarcloud.io/project/configuration/GitHubManual?id=<project_name>
+   */
   constructor(private readonly opts: SonarScannerOpts) {}
 
+  /**
+   * Runs `opts.run.runTestsCmd`
+   */
   async runTests(): Promise<void> {
     await this.opts.run.runTestsCmd;
   }
 
-  async uploadTests(): Promise<void> {
+  /**
+   * Upload test coverage to SonarQube.
+   */
+  async uploadTestCoverage(): Promise<void> {
     if (!(await isCmdAvailable("sonar"))) {
       console.log("Sonar is missing, installing globally via NPM...");
       await $`npm i -g @sonar/scan`;
