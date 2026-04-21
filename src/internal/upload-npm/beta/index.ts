b965a2bf8614acf4c9004e7f65321d55dcc19297
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-21 01:18:36 -0400

    setup npm beta uploads via CD in pull requests

---
+++ src/internal/upload-npm/beta/index.ts
@@ -0,0 +1,53 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import { GitHubClient } from "../../../gh";
+import { NPMClient } from "../../../npm";
+import { Utils } from "../../../utils";
+
+const { sha } = await yargs(hideBin(process.argv))
+  .option("sha", {
+    type: "string",
+    demandOption: true,
+  })
+  .strict()
+  .parse();
+
+async function main() {
+  const ghClient = await GitHubClient.createWithDefaultCiToken();
+  const npmClient = await NPMClient.create();
+
+  const shortSha = await getShortSha(sha);
+
+  const latestTag = ghClient.getLatestTag();
+
+  const betaVersion = `${latestTag}-beta.${shortSha}`;
+
+  if (!Utils.SemVer.validate(betaVersion)) {
+    throw new Error(`Generated invalid beta version: ${betaVersion}`);
+  }
+
+  await Utils.updateAllPackageJsonsWithVersion(betaVersion);
+  await npmClient.publish(false, true);
+
+  console.log(`Uploaded ${betaVersion} to NPM.`);
+}
+
+async function getShortSha(sha: string) {
+  const shortSha = sha.slice(0, 8).toString().trim();
+
+  if (shortSha.length !== 8) {
+    throw new Error("Could not parse git SHA");
+  }
+
+  return shortSha;
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
